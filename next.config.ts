06cfb016a633086a90de7c51fa2d0d04d8bdacf7
sakir777 <sakirshaikh0777@gmail.com>
2025-09-20 16:51:14 +0530

    fix: image config domains added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "images.unsplash.com",       // for Unsplash images
+      "avatars.githubusercontent.com" // for GitHub avatars
+    ],
+  },
 };
 
 export default nextConfig;
